41db31e81d64a4eaa47ef61f20525fdb46526d8f
nsanchez <nsanchez@106-10.info2>
2023-10-03 13:31:55 +0200

    Ejercicio 1 - Tema 2: Lote 1
    NSS

---
+++ src/ejercicio01/Ejercicio01.java
@@ -0,0 +1,30 @@
+package ejercicio01;
+
+import java.util.Scanner;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		//Registra la variable num, para almacenar el numero
+		int num;
+		//Inicia el Scanner para registrar los datos por el teclado
+		Scanner sc = new Scanner(System.in);
+		//Se le pedira al usuario que introduzca un numero
+		System.out.println("Introduce un numero");
+		//El numero introducido se guardara en la variable num
+		num = sc.nextInt();
+		//Si el resto del numero entre 2 es 1...
+		if (num%2==1) {
+			//Se le informara al usuario que el numero introducido es impar
+			System.out.println("El numero es impar");
+		}
+		//Si no es asi...
+		else {
+			//Se le informara al usuario que el numero introducido es par
+			System.out.println("El numero es par");
+		}
+		//Cierra el Scanner
+		sc.close();
+	}
+
+}
