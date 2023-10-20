e26d4f2a2ca06bd380ef206813c75ca4492e5fcb
nestorss05 <nestor.sanchez@iesnervion.es>
2023-10-20 14:34:28 +0200

    Ejercicios T2 Lote 4 (Do While) NSS

--- src/ejerciciosT2_4/EjercicioT2_4_05.java
+++ src/ejerciciosT2_4/EjercicioT2_4_05.java
@@ -1,17 +1,24 @@
 package ejerciciosT2_4;
 
-import java.util.Random;
+import java.util.Random;	
 import java.util.Scanner;
 
 public class EjercicioT2_4_05 {
 
 	public static void main(String[] args) {
 
-		// aleatorio: estatica a la que se guardara el numero aleatorio
+		// aleatorio: variable a la que se guardara el numero aleatorio
 		int numeroAleatorio;
 
-		// numero: respuesta del usuario
-		int numero = 0;
+		// numeroMinimo: numero minimo que podria adivinar la maquina. Por defecto es 1
+		int numeroMinimo = 1;
+
+		// numeroMaximo: numero minimo que podria adivinar la maquina. Por defecto es
+		// 101
+		int numeroMaximo = 101;
+
+		// respuesta: la respuesta del ordenador
+		String respuesta;
 
 		// Abre el Scanner y guardalo en la variable sc
 		Scanner sc = new Scanner(System.in);
@@ -19,29 +26,49 @@ public class EjercicioT2_4_05 {
 		// Abre el Random y guardalo en la variable aleatorio
 		Random aleatorio = new Random();
 
+		//Informale que solo adivinaras numeros del 1 al 100
+		System.out.println("Piensa en un numero del 1 al 100. Yo procedere a adivinarlo. ");
+		
 		/*
-		 * Bucle: Mientras que el numero no haya sido adivinado, se le preguntara
-		 * continuamente al usuario por un numero
+		 * Bucle: Mientras que el numero no haya sido adivinado por el ordenador, se le
+		 * preguntara continuamente al usuario por un numero
 		 */
 
 		do {
 
-			// Se le asignara a ALEATORIO un numero aleatorio entre 1 y 100, pero continuamente
-			numeroAleatorio = aleatorio.nextInt(1, 101);
-			System.out.println("Adivina un numero");
-			numero = sc.nextInt();
-			if (numero < numeroAleatorio) {
-				System.out.println("Mayor");
+			// Se le asignara a ALEATORIO un numero aleatorio entre 1 y 100, pero
+			// continuamente, hasta que la maquina lo adivine
+
+			numeroAleatorio = aleatorio.nextInt(numeroMinimo, numeroMaximo);
+
+			// Te dira que el numero que penso, y te preguntara si vale o no
+			System.out.println("He pensado en el numero " + numeroAleatorio + ". ¿Es igual, es mayor o es menor?");
+			respuesta = sc.nextLine().toUpperCase();
+
+			if (respuesta.equals("MAYOR")) {
+				numeroMinimo = numeroAleatorio+1;
+			} else if (respuesta.equals("MENOR")) {
+				numeroMaximo = numeroAleatorio;
+			} else if (!respuesta.equals("IGUAL")){
+				System.out.println("Respuesta invalida.");
 			}
-			if (numero > numeroAleatorio) {
-				System.out.println("Menor");
-			}
 
-		} while (numero != numeroAleatorio); // Fin del bucle
+		} while ((!respuesta.equals("IGUAL")) && numeroMinimo != numeroMaximo);
+		// Fin del bucle
 
-		//Se informara que los numeros son iguales y que has ganado
-		System.out.println("Igual. GG");
-
+		// If: si el numero minimo es igual que el maximo
+		if (numeroMinimo == numeroMaximo) {
+			
+			// Se informara que eres un troleador cara
+			System.out.println("Eres un troleador cara");
+			
+		} else {
+			
+			// Se informara que los numeros son iguales y que has ganado
+			System.out.println("Ja, lo he adivinado");
+			
+		} // Fin del If
+		
 		// Cierra el Scanner
 		sc.close();
 
